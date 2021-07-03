fc19555fa2326f7c7e320b4bbc8ac0c2319cf552
Marvin Löbel <loebel.marvin@gmail.com>
2021-07-04 00:57:57 +0200

    foo

---
+++ do_macro/src/lib.rs
@@ -0,0 +1,10 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_attribute]
+pub fn do_scope(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use do_macro::do_scope;
+use std::ops::ControlFlow;
+
+macro_rules! do_block {
+    (do $b:block) => {
+        || ControlFlow::Continue($b)
+    };
+}
+
+#[do_scope]
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let tmp = foo(
+            do_block!(do {
+                if true {
+                    return ControlFlow::Break(MainJumpTargets::Return);
+                }
+                if true {
+                    return ControlFlow::Break(MainJumpTargets::Break);
+                }
+                if true {
+                    return ControlFlow::Break(MainJumpTargets::Continue);
+                }
+            }),
+            do_block!(do {
+
+            }),
+        );
+        if let ControlFlow::Break(dst) = tmp {
+            match dst {
+                MainJumpTargets::Return => return,
+                MainJumpTargets::Break => break,
+                MainJumpTargets::Continue => continue,
+            }
+        }
+    }
+}
+
+enum MainJumpTargets {
+    Return,
+    Break,
+    Continue,
+}
+
+fn foo<B>(
+    mut f: impl FnMut() -> ControlFlow<B>,
+    mut g: impl FnMut() -> ControlFlow<B>,
+) -> ControlFlow<B> {
+    f()?;
+    g()
 }
