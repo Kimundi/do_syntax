0c08bcc3a56200668e0adeaf371a7f2ef2fa46a8
Marvin Löbel <loebel.marvin@gmail.com>
2021-07-04 18:26:15 +0200

    snap

--- do_macro_impl/src/lib.rs
+++ do_macro_impl/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
 use syn;
 use syn::fold::{self, Fold};
@@ -26,26 +27,70 @@ impl syn::parse::Parse for Function {
     }
 }
 
+#[derive(Debug)]
+struct DoMacro {
+    func: syn::ExprCall,
+    block: syn::ExprBlock,
+}
+
+impl syn::parse::Parse for DoMacro {
+    fn parse(input: syn::parse::ParseStream) -> syn::parse::Result<Self> {
+        println!("{:?}", &input);
+        let r = DoMacro {
+            func: input.parse()?,
+            block: input.parse()?,
+        };
+        println!("{:?}", &r);
+        Ok(r)
+    }
+}
+
 struct State {
-    stack: Vec<String>,
+    stack: Vec<StackEntry>,
+}
+
+enum StackEntry {
+    Function,
+    JumpTarget { label: Option<syn::Lifetime> },
+}
+
+impl std::fmt::Debug for StackEntry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            StackEntry::Function => write!(f, "function"),
+            StackEntry::JumpTarget { label } => write!(f, "loop {:?}", label),
+        }
+    }
 }
 
 impl State {
     fn new() -> Self {
-        Self { stack: Vec::new() }
+        Self {
+            stack: vec![StackEntry::Function],
+        }
+    }
+
+    fn parse_macro(&mut self, tokens: TokenStream) -> TokenStream {
+        let input = syn::parse_macro_input!(tokens as DoMacro);
+
+        let expr = input.block;
+        let expanded = quote! {
+            #expr
+        };
+
+        TokenStream::from(expanded)
     }
 
     fn replace_expr(&mut self, i: Expr) -> Expr {
         if let Expr::Macro(i) = &i {
-            println!("--------");
+            println!("/*");
             println!("{:#?}", self.stack);
             println!("{}", i.to_token_stream());
-            println!("--------");
+            println!("*/");
             println!();
             if i.mac.path.is_ident("do_") {
-                return Expr::Verbatim(quote! {
-                    42
-                });
+                let transformed = self.parse_macro(i.mac.tokens.clone().into());
+                return Expr::Verbatim(transformed.into());
             }
         }
         i
@@ -56,36 +101,58 @@ impl Fold for State {
     fn fold_expr(&mut self, i: Expr) -> Expr {
         let mut pushed = false;
         match &i {
-            Expr::Block(i) => {
-                self.stack.push(format!("Block"));
-                pushed = true;
-            }
-            Expr::Break(i) => {
-                self.stack.push(format!("Break"));
-                pushed = true;
-            }
+            // reset of stack
+            // TODO: ItemFN, ItemMethod
             Expr::Closure(i) => {
-                self.stack.push(format!("Closure"));
-                pushed = true;
+                /*
+                    self.stack.push(format!("Closure"));
+                    pushed = true;
+                */
+            }
+
+            // jumps
+            Expr::Break(i) => {
+                /*
+                    self.stack.push(format!("Break"));
+                    pushed = true;
+                */
             }
             Expr::Continue(i) => {
-                self.stack.push(format!("Continue"));
+                /*
+                    self.stack.push(format!("Continue"));
+                    pushed = true;
+                */
+            }
+            Expr::Return(i) => {
+                /*
+                    self.stack.push(format!("Return"));
+                    pushed = true;
+                */
+            }
+
+            // jump targets
+            Expr::Block(i) => {
+                self.stack.push(StackEntry::JumpTarget {
+                    label: i.label.as_ref().map(|v| v.name.clone()),
+                });
                 pushed = true;
             }
             Expr::ForLoop(i) => {
-                self.stack.push(format!("ForLoop"));
+                self.stack.push(StackEntry::JumpTarget {
+                    label: i.label.as_ref().map(|v| v.name.clone()),
+                });
                 pushed = true;
             }
             Expr::Loop(i) => {
-                self.stack.push(format!("Loop"));
-                pushed = true;
-            }
-            Expr::Return(i) => {
-                self.stack.push(format!("Return"));
+                self.stack.push(StackEntry::JumpTarget {
+                    label: i.label.as_ref().map(|v| v.name.clone()),
+                });
                 pushed = true;
             }
             Expr::While(i) => {
-                self.stack.push(format!("While"));
+                self.stack.push(StackEntry::JumpTarget {
+                    label: i.label.as_ref().map(|v| v.name.clone()),
+                });
                 pushed = true;
             }
             _ => {}
@@ -96,5 +163,9 @@ impl Fold for State {
             self.stack.pop().unwrap();
         }
         ret
+    }
+
+    fn fold_item_fn(&mut self, i: ItemFn) -> ItemFn {
+        fold::fold_item_fn(self, i)
     }
 }
